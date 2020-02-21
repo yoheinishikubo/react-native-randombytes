9b7d38e9384cc6c365996d1f4f14f04ecb94782e
yoheinishikubo <yohei@me.com>
2020-02-21 17:07:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-if (typeof Buffer === 'undefined') {
-  global.Buffer = require('buffer').Buffer
+if (typeof Buffer === "undefined") {
+  global.Buffer = require("buffer").Buffer;
 }
 
-import * as Random from 'expo-random'
+import * as Random from "expo-random";
 
 export const RNS_MAX = 1024;
 
 const max = (a, b) => (a < b ? b : a);
 let RNS = Buffer.allocUnsafe(0);
 
+function noop() {}
 
-function noop () {}
-
-function toBuffer (nativeStr) {
-  return new Buffer(nativeStr, 'base64')
+function toBuffer(nativeStr) {
+  return new Buffer(nativeStr, "base64");
 }
 
 export async function init() {
@@ -29,19 +28,25 @@ async function generate(byteCount) {
   RNS = Buffer.concat([RNS, Buffer.from(buff)]);
 }
 
-export function randomBytes (length, cb) {
+export function randomBytes(length, cb) {
   if (!cb) {
+    if (length <= RNS.length) {
       const ret = Buffer.from(RNS.slice(0, length));
       RNS = RNS.slice(length, RNS.length - length);
 
       generate(max(0, RNS_MAX - RNS.length));
 
       return ret;
+    } else {
+      throw new Error("Should be prepare enough random numbers");
+    }
   }
 
-  Random.getRandomBytesAsync(length).then(randomBytes=>{
-    cb(null, Buffer.from(randomBytes));
-  }).catch(cb);
+  Random.getRandomBytesAsync(length)
+    .then(randomBytes => {
+      cb(null, Buffer.from(randomBytes));
+    })
+    .catch(cb);
 }
 
-init()
+init();
